a1f2140ee1816d790c605292a3ce028ff113707e
Max Hille <mh@lambdasoup.com>
2023-01-23 17:10:16 +0100

    run minimal sample without conda

---
+++ render.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+python3 -m venv venv
+source venv/bin/activate
+#pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+# https://stackoverflow.com/a/73582465
+export HSA_OVERRIDE_GFX_VERSION=10.3.0
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/rocm5.2
+pip install git+https://github.com/openai/CLIP.git@d50d76daa670286dd6cacf3bcd80b5e4823fc8e1
+pip install einops
+pip install pytorch_lightning
+pip install diffusers
+pip install transformers
+pip install omegaconf
+pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+pip install kornia
+pip install invisible-watermark
+pip install opencv-python
+
+echo Running TXT2IMG
+
+python txt2img.py --ckpt ~/v1-5-pruned-emaonly.ckpt --n_samples 1 --n_iter 1 --H 256 --W 256 --prompt "a turtle with a horse on the back, by greg ruthkowski" 
--- txt2img.py
+++ txt2img.py
@@ -2,10 +2,10 @@ import argparse, os, sys, glob
 import cv2
 import torch
 import numpy as np
-from omegaconf import OmegaConf
 from PIL import Image
-from tqdm import tqdm, trange
+from omegaconf import OmegaConf
 from imwatermark import WatermarkEncoder
+from tqdm import tqdm, trange
 from itertools import islice
 from einops import rearrange
 from torchvision.utils import make_grid
