e7c8f7ad654a5f6a4de84166b6613db99b7ae8ab
Max Hille <mh@lambdasoup.com>
2023-01-26 09:27:01 +0100

    remove torchaudio dep

--- render.sh
+++ render.sh
@@ -8,7 +8,7 @@ pip install pip==22.3.1
 #pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
 # https://stackoverflow.com/a/73582465
 export HSA_OVERRIDE_GFX_VERSION=10.3.0
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/rocm5.2
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm5.2
 pip install git+https://github.com/openai/CLIP.git@d50d76daa670286dd6cacf3bcd80b5e4823fc8e1
 pip install einops
 pip install pytorch_lightning
